fbd17297c69cbe389236c82fcaa2de951928d383
hj2016github <linzhiqiu@gmail.com>
2018-09-20 11:12:26 +0800

    修改了下载后,第一次取消-->只要不关闭app,第二次启动直接安装报空的 问题;

--- app/src/main/java/com/gehj/okhttp_netframe/MainActivity.java
+++ app/src/main/java/com/gehj/okhttp_netframe/MainActivity.java
@@ -1,9 +1,18 @@
 package com.gehj.okhttp_netframe;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +25,7 @@ import com.gehj.okhttp_netframe.db.DownloadEntity;
 import com.gehj.okhttp_netframe.http.DownLoadCallback;
 import com.gehj.okhttp_netframe.http.HttpManger;
 import com.gehj.okhttp_netframe.http.OkhttpDownloader;
+import com.gehj.okhttp_netframe.utils.FileStorageManger;
 import com.gehj.okhttp_netframe.utils.GlobeUrl;
 import com.gehj.okhttp_netframe.utils.Singleton;
 
@@ -33,15 +43,15 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Data";
     private ImageView imageView;
-    private int count;
     private ProgressBar progressBar;
     private TextView textView;
     private Button button;
-    private DownloadEntity downloadEntity = Singleton.getDownloadEntityInstance();;
+    private DownloadEntity downloadEntity = Singleton.getDownloadEntityInstance();
+    ;
     private long id_download;
     private OkhttpDownloader downloader;
-    private long breakPoints = 0;    //断点;
-    private long totalBytes; //文件总长度;
+    private File apk;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
         mangeDownload();//下载完成后控制不能第二次下载;只能进行安装;
 
 
-
     }
 
     private void initDownload() {
@@ -79,34 +88,32 @@ public class MainActivity extends AppCompatActivity {
         boolean success = LitePal.find(DownloadEntity.class, id_download).isSuccess();
         if (!success) { //第一次下载
             download_app(0l);//下载apk;
-        }else {
+        } else {
             button.setEnabled(false);//下载完成设置按钮不可点击;
             button.setClickable(false);
             progressBar.setProgress(100);//下载成功后显示进度条;
-            //TODO 进行安装等;1,比较contentlength 2,进行安装;
+            File file = FileStorageManger.getInstance().getFileByName(GlobeUrl.apkUrl);//success保证只有下载完整才能调这个file;
+           if (LitePal.find(DownloadEntity.class,downloadEntity.getId()).isSuccess()) doInstallAuthority(file);
 
         }
     }
 
 
-    private void download_app(long startPoint){
-        downloader.asyncRequestBreakPointDownLoadFile(GlobeUrl.apkUrl,startPoint, new DownLoadCallback() {
+    private void download_app(long startPoint) {
+        downloader.asyncRequestBreakPointDownLoadFile(GlobeUrl.apkUrl, startPoint, new DownLoadCallback() {
             @Override
             public void success(File file) {
-                Log.e(TAG, "success: "+file.getName()+": "+file.length() );
-                Log.e(TAG, "success: "+file.getAbsolutePath() );
-                /*final Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        imageView.setImageBitmap(bitmap);
-                    }
-                });*/
+                Log.e(TAG, "success: " + file.getName() + ": " + file.length());
+                Log.e(TAG, "success: " + file.getAbsolutePath());
+               if(LitePal.find(DownloadEntity.class,downloadEntity.getId()).isSuccess())
+                   doInstallAuthority(file);
+
             }
+
 
             @Override
             public void fail(int errorCode, String errorMessage) {
-                Log.e(TAG, "fail: "+errorMessage );
+                Log.e(TAG, "fail: " + errorMessage);
             }
 
             @Override
@@ -122,8 +129,19 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void doInstallAuthority(File file) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.REQUEST_INSTALL_PACKAGES}, 1);//sdk>26 android8.0以上需要有REQUEST_INSTALL_PACKAGES权限;
+            apk = file; //先传完整的file,才进行回调;
+        } else {
+            installApk(file);
+        }
+    }
+
     private void download_1() {
-        OkhttpDownloader.getInstance().asyncRequestBreakPointDownLoadFile(GlobeUrl.picUrl,0, new DownLoadCallback() {
+        OkhttpDownloader.getInstance().asyncRequestBreakPointDownLoadFile(GlobeUrl.picUrl, 0, new DownLoadCallback() {
             @Override
             public void success(File file) {
                 final Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
@@ -149,43 +167,69 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void getData() {
-            HttpManger.getInstance().getAsyncRequest(GlobeUrl.url, new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
+    private void getData() {//下载json等;
+        HttpManger.getInstance().getAsyncRequest(GlobeUrl.url, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
 
-                }
+            }
 
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                        final String text = response.body().string();
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                textView.setText(text);
-                            }
-                        });
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String text = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(text);
+                    }
+                });
 
-                }
-            });
-
+            }
+        });
 
 
     }
 
 
     public void pause(View view) {
-        boolean isCancel = LitePal.find(DownloadEntity.class,id_download).isPause();//初始是false;
-        if (isCancel){
+        boolean isCancel = LitePal.find(DownloadEntity.class, id_download).isPause();//初始是false;
+        if (isCancel) {
             downloadEntity.setToDefault("isPause");//litepal改回默认值用此方法;
             downloadEntity.update(downloadEntity.getId());
-            long startPos =  LitePal.find(DownloadEntity.class,id_download).getProgress_pos();
-            download_app(startPos+1);
-        }else {
+            long startPos = LitePal.find(DownloadEntity.class, id_download).getProgress_pos();
+            download_app(startPos + 1);//startPos从+1处开始;
+        } else {
             downloadEntity.setPause(true);
             downloadEntity.update(downloadEntity.getId());
             downloader.pause();
         }
     }
 
+    private void installApk(File file) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri =null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            uri  = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID+".okhttpdownload", file);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        } else {
+            uri =  Uri.fromFile(file);
+        }
+        intent.setDataAndType(uri,"application/vnd.android.package-archive");
+        startActivity(intent);
+    }
+
+
+    /*回调运行时权限*/
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                        grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                    installApk(apk);//回调总是在传file(完整)之后,所以apk不会空;
+                }
+                break;
+        }
+    }
 }
